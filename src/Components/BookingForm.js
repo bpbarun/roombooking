7abff33be9f87e56b460c3b334a850b0fbc824f1
barun2.pandey <barun2.pandey@ril.com>
2024-10-29 13:42:11 +0530

    addign booking form

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,61 +1,71 @@
 import React from "react";
 import './bookingform.css'
+import {IP} from './Constant'
 const BookingForm = ()=>{
     return(
         <>
         
     <h2>Add Your Information:</h2>
-
-<form>
-    <div class="form-group row">
-        <div>
-            <label for="name">Name <span class="mandatory">MANDATORY</span></label>
-            <input type="text" id="name" name="name" required />
-        </div>
-        <div>
-            <label for="surname">Surname <span class="mandatory">MANDATORY</span></label>
-            <input type="text" id="surname" name="surname" required />
+<div className="row">
+    <div className="col-4">
+        <div className="bg-thumbnail bg-img firstImage">
+            <img className="firstImage" src={IP +`/rooms-image/`+'1.png'} alt=""/>
         </div>
     </div>
+    <div className="col-8">
+        <form>
+        <div class="form-group row">
+            <div>
+                <label for="name">Name <span class="mandatory">MANDATORY</span></label>
+                <input type="text" id="name" name="name" required />
+            </div>
+            <div>
+                <label for="surname">Surname <span class="mandatory">MANDATORY</span></label>
+                <input type="text" id="surname" name="surname" required />
+            </div>
+        </div>
 
-    <div class="form-group row">
-        <div>
-            <label for="email">Email <span class="mandatory">MANDATORY</span></label>
-            <input type="email" id="email" name="email" required />
+        <div class="form-group row">
+            <div>
+                <label for="email">Email <span class="mandatory">MANDATORY</span></label>
+                <input type="email" id="email" name="email" required />
+            </div>
+            <div>
+                <label for="telephone">Telephone <span class="mandatory">MANDATORY</span></label>
+                <input type="tel" id="telephone" name="telephone" required />
+            </div>
         </div>
-        <div>
-            <label for="telephone">Telephone <span class="mandatory">MANDATORY</span></label>
-            <input type="tel" id="telephone" name="telephone" required />
-        </div>
-    </div>
 
-    <div class="form-group row">
-        <div>
-            <label for="address">Address</label>
-            <input type="text" id="address" name="address" />
+        <div class="form-group row">
+            <div>
+                <label for="address">Address</label>
+                <input type="text" id="address" name="address" />
+            </div>
+            <div>
+                <label for="city">City</label>
+                <input type="text" id="city" name="city" />
+            </div>
         </div>
-        <div>
-            <label for="city">City</label>
-            <input type="text" id="city" name="city" />
-        </div>
-    </div>
 
-    <div class="form-group row">
-        <div>
-            <label for="country">Country</label>
-            <input type="text" id="country" name="country" />
+        <div class="form-group row">
+            <div>
+                <label for="country">Country</label>
+                <input type="text" id="country" name="country" />
+            </div>
+            <div>
+                <label for="zip">ZIP</label>
+                <input type="text" id="zip" name="zip" />
+            </div>
         </div>
-        <div>
-            <label for="zip">ZIP</label>
-            <input type="text" id="zip" name="zip" />
-        </div>
-    </div>
 
-    <div class="form-group">
-        <label for="requests">Requests</label>
-        <input type="text" id="requests" name="requests" />
+        <div class="form-group">
+            <label for="requests">Checkout</label>
+            <input type="button" id="requests" className="primary"/>
+        </div>
+    </form>
     </div>
-</form>
+</div>
+
         </>
     )
 }
